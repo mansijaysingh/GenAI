cc85982591a2ee43b59c5a1babdc60b631823742
Mansijaysingh <mansisingh.am000@gmail.com>
2026-02-19 14:10:25 +0530

    chapter3

---
+++ Python/datatypes/chapter_3.py
@@ -0,0 +1,30 @@
+#INterger
+
+black_tea_grams=12
+ginger_grams=5
+
+total_grams=black_tea_grams+ginger_grams
+print(f"Total grams of tea is:{total_grams}")
+
+remaing_tea=black_tea_grams-ginger_grams
+print(f"Remaining grams of tea is:{remaing_tea}")
+
+milk_litres=7
+serving=4
+milk_per_serving=milk_litres/serving
+print(f"Milk per serving is:{milk_per_serving} litres")
+
+total_tea_bags=7
+pots=3
+tea_bags_per_pot=total_tea_bags//pots
+print(f"Tea bags per pot is:{tea_bags_per_pot}")
+
+total_cadamom_pods=10
+pods_per_cup=3
+leftover_pods=total_cadamom_pods/pods_per_cup
+print(f"Leftover cardamom pods is:{leftover_pods}")
+
+base_flavor_strength=2
+scale_factor=3
+powerful_flavor=base_flavor_strength**scale_factor
+print(f"Powerful flavor strength is:{powerful_flavor}")
