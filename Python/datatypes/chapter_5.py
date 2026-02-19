578069188dcff5cb8af86a2a9bd4020f418174f0
Mansijaysingh <mansisingh.am000@gmail.com>
2026-02-19 14:56:28 +0530

    chapter5

---
+++ Python/datatypes/chapter_5.py
@@ -0,0 +1,13 @@
+#Real number
+import sys
+from fractions import Fraction
+from decimal import Decimal
+
+
+ideal_temp=95.5
+current_temp=95.49999999999
+
+print(f"is Ideal temp {ideal_temp}")
+print(f"Is current temp {current_temp}")
+print(f"diffrence between ideal and current temp {ideal_temp-current_temp}")
+
