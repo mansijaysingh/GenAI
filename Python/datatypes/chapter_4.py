59e7fc7a7f42ce7f36ce49a6ec0dc970f5cb3bcf
Mansijaysingh <mansisingh.am000@gmail.com>
2026-02-19 14:52:02 +0530

    chapter4

---
+++ Python/datatypes/chapter_4.py
@@ -0,0 +1,16 @@
+#Boolean
+
+is_boiling=True
+print(f"Is the water boiling? {is_boiling}")
+
+milk_present=0
+print(f"Is milk present?{bool(milk_present)}")
+
+
+#Logical Operation
+
+water_hot=True
+tea_added=False
+
+can_server=water_hot and tea_added
+print(f"Can I serve the tea? {can_server}")
