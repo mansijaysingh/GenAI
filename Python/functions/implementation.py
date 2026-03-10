a590367efa1bfe414ce11c76363974cf072fdd09
Mansijaysingh <mansisingh.am000@gmail.com>
2026-03-10 12:49:14 +0530

    implementation

---
+++ Python/functions/implementation.py
@@ -0,0 +1,17 @@
+def get_input():
+  print("getting user input...")
+
+
+def validate_input():
+  print("validating user input...")
+
+def save_to_db():
+  print("saving data to the database...")
+
+def register_user():
+  get_input()
+  validate_input()
+  save_to_db()
+  print("user registered successfully.")
+
+register_user()
