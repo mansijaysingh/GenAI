5f4fa1a32c17d9f68e8e412841b9176864329c90
Mansijaysingh <mansisingh.am000@gmail.com>
2026-03-10 12:44:51 +0530

    function basics

---
+++ Python/functions/complex.py
@@ -0,0 +1,16 @@
+def fetch_sales():
+  print("Fetching sales data from the database...")
+
+def filter_valid_sales():
+  print("Filtering valid sales records...")
+
+def summarize_data():
+  print("Summarizing sales data...")
+
+def generate_report():
+  fetch_sales()
+  filter_valid_sales()
+  summarize_data()
+  print("report generated successfully.")
+
+generate_report()
