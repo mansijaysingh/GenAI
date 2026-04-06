809ec0fd82449214ff2f7f84ea0fa979258c8713
Mansijaysingh <mansisingh.am000@gmail.com>
2026-04-06 12:35:12 +0530

    setting up redis and valkey with docker

---
+++ RAG & Langchain/RAG_queue/queue/worker.py
@@ -0,0 +1,45 @@
+from openai import OpenAI
+from langchain_openai import OpenAIEmbeddings
+from langchain_qdrant import QdrantVectorStore
+from dotenv import load_dotenv
+
+load_dotenv()
+
+client=OpenAI()
+
+embedding_model=OpenAIEmbeddings(
+  model="text-embedding-3-large"
+)
+
+vector_db=QdrantVectorStore.from_existing_collection(
+  url="http://localhost:6333",
+  collection_name="learning-collection",
+  embedding=embedding_model
+)
+
+def process_query(query:str):
+  print("Serching chunks",query)
+  search_results=vector_db.similarity_search(query=query)
+
+  context = "\n\n\n".join([f"Page Content: {result.page_content}\nPage Number: {result.metadata['page_label']}\nFile Location: {result.metadata['source']}" for result in search_results])
+
+  SYSTEM_PROMPT = f"""
+    You are a helpfull AI Assistant who answeres user query based on the available context retrieved from a PDF file along with page_contents and page number.
+
+    You should only ans the user based on the following context and navigate the user to open the right page number to know more.
+
+    Context:
+    {context}
+    """
+
+  response = client.chat.completions.create(
+        model="gpt-5",
+        messages=[
+            { "role": "system", "content":SYSTEM_PROMPT  },
+            { "role": "user", "content":query  },
+        ]
+    )
+
+  print(f"🤖: {response.choices[0].message.content}")
+  return response.choices[0].message.content
+
