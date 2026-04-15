99f6ebf7b335fcb11cbf9a5c652a9652eee51dda
Mansijaysingh <mansisingh.am000@gmail.com>
2026-04-15 14:14:48 +0530

    connecting nodes with edges- Designing complex Ai graph

--- LangGraph/chat.py
+++ LangGraph/chat.py
@@ -1,7 +1,7 @@
 from typing_extensions import TypedDict
 from typing import Annotated
 from langgraph.graph.message import add_messages
-from langgraph.graph import StateGraph
+from langgraph.graph import StateGraph , START, END
 
 
 
@@ -10,10 +10,11 @@ class State(TypedDict):
 
 
 def chatbot(state:State):
-
+  print("Chatbot node called with state:", state)
   return {"messages":["Hi! How can I help you?"]}
 
 def samplenode(state:State):
+  print("Sample node called with state:", state)
 
   return {"messages":["This is a sample node."]}
 
@@ -21,5 +22,11 @@ graph_builder=StateGraph(State)
 
 graph_builder.add_node("chatbot", chatbot)
 graph_builder.add_node("samplenode", samplenode)
+
+graph_builder.add_edge(START, "chatbot")
+graph_builder.add_edge("chatbot", "samplenode")
+graph_builder.add_edge("samplenode", END)
+
+graph=graph_builder.compile()
 
 
