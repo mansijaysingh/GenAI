833e2953c4ec010df4e426cd49ba2f71d9cf8721
Mansijaysingh <mansisingh.am000@gmail.com>
2026-04-15 14:09:20 +0530

    defining nodes and functions in langGraph

--- LangGraph/chat.py
+++ LangGraph/chat.py
@@ -9,5 +9,17 @@ class State(TypedDict):
   message:Annotated[list,add_messages]
 
 
+def chatbot(state:State):
+
+  return {"messages":["Hi! How can I help you?"]}
+
+def samplenode(state:State):
+
+  return {"messages":["This is a sample node."]}
+
 graph_builder=StateGraph(State)
+
+graph_builder.add_node("chatbot", chatbot)
+graph_builder.add_node("samplenode", samplenode)
+
 
