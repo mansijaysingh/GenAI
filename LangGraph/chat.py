06966985f761672e74bff3806dbed1a8ddfc8706
Mansijaysingh <mansisingh.am000@gmail.com>
2026-04-15 14:37:50 +0530

    testing and debugging langGraph AI workflow

--- LangGraph/chat.py
+++ LangGraph/chat.py
@@ -5,18 +5,18 @@ from langgraph.graph import StateGraph , START, END
 
 
 
+
 class State(TypedDict):
   message:Annotated[list,add_messages]
 
 
 def chatbot(state:State):
-  print("Chatbot node called with state:", state)
-  return {"messages":["Hi! How can I help you?"]}
+  print("\n\nChatbot node called with state:", state)
+  return {"message":["Hi! How can I help you?"]}
 
 def samplenode(state:State):
-  print("Sample node called with state:", state)
-
-  return {"messages":["This is a sample node."]}
+  print("\n\nSample node called with state:", state)
+  return {"message":["This is a sample node."]}
 
 graph_builder=StateGraph(State)
 
@@ -29,4 +29,9 @@ graph_builder.add_edge("samplenode", END)
 
 graph=graph_builder.compile()
 
+# updated_state=graph.invoke(State({"messages":["Hi, My name is Mansi..."]}))
+# print("Updated state after graph execution:", updated_state)
 
+initial_input={"message": ["Hi, My name is Mansi..."]}
+updated_state=graph.invoke(initial_input)
+print("\n\nUpdated state after graph execution:", updated_state)
