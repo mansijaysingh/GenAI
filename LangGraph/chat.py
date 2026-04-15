90c080b2fed2f5cc1dc9a99a83892092e799a72e
Mansijaysingh <mansisingh.am000@gmail.com>
2026-04-15 14:01:59 +0530

    defining state  in langgraph for Ai agent context

---
+++ LangGraph/chat.py
@@ -0,0 +1,13 @@
+from typing_extensions import TypedDict
+from typing import Annotated
+from langgraph.graph.message import add_messages
+from langgraph.graph import StateGraph
+
+
+
+class State(TypedDict):
+  message:Annotated[list,add_messages]
+
+
+graph_builder=StateGraph(State)
+
