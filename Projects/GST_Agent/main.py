bbad4f8e8b08e7f0ec59934f88e1224a156ac3fd
Mansijaysingh <mansisingh.am000@gmail.com>
2026-04-23 13:23:57 +0530

    feat: initial project setup and file upload API implementation

---
+++ Projects/GST_Agent/main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI, UploadFile,File
+import shutil
+import os
+
+
+app=FastAPI()
+
+UPLOAD_DIR="uploads"
+if not os.path.exists(UPLOAD_DIR):
+  os.makedirs(UPLOAD_DIR)
+
+
+@app.post("/upload-bill")
+async def upload_bill(file:UploadFile=File(...)):
+  file_location=f"{UPLOAD_DIR}/{file.filename}"
+  with open(file_location, "wb+") as file_object:
+    shutil.copyfileobj(file.file, file_object)
+
+  return {"info": f"File '{file.filename}' successfully saved at '{file_location}'"}
